f1cb5ba26344affbdda8e00955ee32557439fc2c
Tengo <tengo.jalaba1@gmail.com>
2023-06-28 21:36:14 +0400

    Update App.js with routing changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
-import {NavbarDiscount} from './components/navbardiscount'
-import {Mainlanding} from './pages/mainlanding'
-import {Footer} from './components/footer/footer'
-import {Cart} from './pages/cart'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { NavbarDiscount } from './components/navbardiscount';
+import { Mainlanding } from './pages/mainlanding';
+import { Footer } from './components/footer/footer';
+import { Cart } from './pages/cart';
 import './App.css';
 import { ShopContextProvider } from "./context/shop-context";
 import { ProductDetails } from './pages/productdetails';
@@ -15,20 +15,18 @@ function App() {
   return (
     <div className="App">
       <ShopContextProvider>
-      <Router>
-        <NavbarDiscount />
+        <Router basename="/DecorationReactJs">
+          <NavbarDiscount />
           <Routes>
-            <Route path="/" element = {<Mainlanding />}/>
-            <Route path="/cart" element = {<Cart />}/>
-            <Route path="/shop"  element = {<Allshoppingpage />}/>
+            <Route path="/" element={<Mainlanding />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/shop" element={<Allshoppingpage />} />
             <Route path="/products/:id" element={<ProductDetails />} />
           </Routes>
-        <Footer />
-      </Router>
-      </ShopContextProvider >
+          <Footer />
+        </Router>
+      </ShopContextProvider>
     </div>
-
-
   );
 }
 
